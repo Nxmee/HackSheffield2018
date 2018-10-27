b5a0cb0a28d26a0f889dbda2acc4f74f3b7df211
Jake Holland <hollandjake@hotmail.co.uk>
2018-10-27 13:19:10 +0100

    Created the basic render function

--- sketch.js
+++ sketch.js
@@ -1,41 +1,38 @@
-let p1,p2,game,time,arena
+let p1,p2,game
 
 function setup() {
-    createCanvas(240, 400);
-    arena = new Array(TILES_HIGH / TILES_WIDE);
-    for (let i = 0; i < arena.length; i++) {
-        arena[i] = new Array(TILES_HIGH / TILES_WIDE).fill(0);
-    }
-    p1 = new Player();
-    p2 = new Player();
-    p1.newPiece();
-    p2.newPiece();
-    game = new Tetris(p1,p2);
+    createCanvas(windowWidth, windowHeight);
+    game = new Tetris();
 }
 
 function draw() {
-    background(0);
     game.process();
+    game.render();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    game.updateSize();
     game.render();
 }
 
 function keyPressed() {
     switch (keyCode) {
         case LEFT_ARROW:
-            p2.moveLeft();
+            game.p2.moveLeft();
         case RIGHT_ARROW:
-            p2.moveRight();
+            game.p2.moveRight();
         case UP_ARROW:
-            p2.rotate();
+            game.p2.rotate();
         case DOWN_ARROW:
-            p2.release();
+            game.p2.release();
         case 65: //A
-            p1.moveLeft();
+            game.p1.moveLeft();
         case 68: //D
-            p1.moveRight();
+            game.p1.moveRight();
         case 87: //W
-            p1.rotate();
+            game.p1.rotate();
         case 83: //S
-            p1.release();
+            game.p1.release();
     }
 }

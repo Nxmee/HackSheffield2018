f2196250cba1f80fa1b9757d56480ac5915dc2ff
Jake Holland <hollandjake@hotmail.co.uk>
2018-10-27 18:08:35 +0100

    Added break

--- sketch.js
+++ sketch.js
@@ -20,19 +20,27 @@ function keyPressed() {
     switch (keyCode) {
         case LEFT_ARROW:
             game.p2.moveLeft();
+            break;
         case RIGHT_ARROW:
             game.p2.moveRight();
+            break;
         case UP_ARROW:
             game.p2.rotate();
+            break;
         case DOWN_ARROW:
             game.p2.release();
+            break;
         case 65: //A
             game.p1.moveLeft();
+            break;
         case 68: //D
             game.p1.moveRight();
+            break;
         case 87: //W
             game.p1.rotate();
+            break;
         case 83: //S
             game.p1.release();
+            break;
     }
 }
